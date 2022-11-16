61edca09c51f4e20255b2f787961e56ebb87a926
Guillermo Durand <guillermo.durand.2011@polytechnique.org>
2022-11-16 19:50:11 +0100

    toy example on sinus data

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 
 
 def main():
@@ -14,14 +16,17 @@ def main():
     home_data = get_data()
 
     app_mode = st.sidebar.selectbox(
-        "Choose the app mode", ["Show instructions", "Regression"]
+        "Choose the app mode",
+        ["Show instructions", "Home data regression", "Sinus regression"],
     )  # , "Show the source code"])
     if app_mode == "Show instructions":
         st.write("To continue select a mode in the selection box to the left.")
     # elif app_mode == "Show the source code":
     #     st.code(get_file_content_as_string("./app.py"))
-    elif app_mode == "Regression":
+    elif app_mode == "Home data regression":
         regression(home_data)
+    elif app_mode == "Sinus regression":
+        sinus()
 
 
 @st.cache
@@ -99,6 +104,55 @@ def regression(home_data):
     val_maes = pd.DataFrame(dict_val_maes).set_index("method")
     st.write(val_maes)
     st.text("(Test what happens when removing TotRmsAbvGrd)")
+
+
+def poly(x, order=3):
+    x_out = x
+    for i in range(2, order + 1):
+        x_out = np.concatenate((x_out, np.power(x, i)), axis=1)
+    return x_out
+
+
+def sinus():
+    # Order of the polynom for the linear regression with polynom
+    order = st.slider(
+        "Choose the order of the polynom for the plynomial regression", 2, 20, 3
+    )
+
+    # Create a random dataset
+    rng = np.random.RandomState(1)
+    X = np.sort(5 * rng.rand(80, 1), axis=0)
+    y = np.sin(X).ravel()
+    y[::5] += 3 * (0.5 - rng.rand(16))
+    X2 = poly(X, order=order)
+
+    # Fit regression models
+    regr_1 = DecisionTreeRegressor(max_depth=2, random_state=1)
+    regr_2 = DecisionTreeRegressor(max_depth=5, random_state=1)
+    regr_3 = LinearRegression()
+    regr_1.fit(X, y)
+    regr_2.fit(X, y)
+    regr_3.fit(X2, y)
+
+    # Predict
+    X_test = np.arange(0.0, 5.0, 0.01)[:, np.newaxis]
+    X2_test = poly(X_test, order=order)
+    y_1 = regr_1.predict(X_test)
+    y_2 = regr_2.predict(X_test)
+    y_3 = regr_3.predict(X2_test)
+
+    # Plot the results
+    fig = plt.figure()
+    plt.scatter(X, y, s=20, edgecolor="black", c="darkorange", label="data")
+    plt.plot(X_test, y_1, color="cornflowerblue", label="max_depth=2", linewidth=2)
+    plt.plot(X_test, y_2, color="yellowgreen", label="max_depth=5", linewidth=2)
+    plt.plot(X_test, y_3, color="red", label="polynom", linewidth=2)
+    plt.xlabel("data")
+    plt.ylabel("target")
+    plt.title("Decision Tree and Polynomial Regression")
+    plt.legend()
+    plt.show()
+    st.pyplot(fig)
 
 
 if __name__ == "__main__":

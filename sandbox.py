5cf0c6238d4512a55222c78c4f2ea5164b879501
Guillermo Durand <guillermo.durand.2011@polytechnique.org>
2022-11-20 23:12:53 +0100

    rearrange sandboxes

--- sandbox.py
+++ sandbox.py
@@ -1,51 +1,85 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-from IPython.display import display
+# import pandas as pd
+# from sklearn.ensemble import RandomForestRegressor
+# from sklearn.metrics import mean_absolute_error
+# from sklearn.model_selection import train_test_split
+# from sklearn.tree import DecisionTreeRegressor
+# from IPython.display import display
 
-iowa_file_path = "./home-data-for-ml-course/train.csv"
+# iowa_file_path = "./home-data-for-ml-course/train.csv"
 
-home_data = pd.read_csv(iowa_file_path)
-# Create target object and call it y
-y = home_data.SalePrice
-# Create X
-features = [
-    "LotArea",
-    "YearBuilt",
-    "1stFlrSF",
-    "2ndFlrSF",
-    "FullBath",
-    "BedroomAbvGr",
-    "TotRmsAbvGrd",
-]
-X = home_data[features]
+# home_data = pd.read_csv(iowa_file_path)
+# # Create target object and call it y
+# y = home_data.SalePrice
+# # Create X
+# features = [
+#     "LotArea",
+#     "YearBuilt",
+#     "1stFlrSF",
+#     "2ndFlrSF",
+#     "FullBath",
+#     "BedroomAbvGr",
+#     "TotRmsAbvGrd",
+# ]
+# X = home_data[features]
 
-display(X.head())
+# display(X.head())
 
-# Split into validation and training data
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+# # Split into validation and training data
+# train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
-# Specify Model
-iowa_model = DecisionTreeRegressor(random_state=1)
-# Fit Model
-iowa_model.fit(train_X, train_y)
-# Make validation predictions and calculate mean absolute error
-val_predictions = iowa_model.predict(val_X)
-val_mae = mean_absolute_error(val_predictions, val_y)
-print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+# # Specify Model
+# iowa_model = DecisionTreeRegressor(random_state=1)
+# # Fit Model
+# iowa_model.fit(train_X, train_y)
+# # Make validation predictions and calculate mean absolute error
+# val_predictions = iowa_model.predict(val_X)
+# val_mae = mean_absolute_error(val_predictions, val_y)
+# print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
 
-# Using best value for max_leaf_nodes
-iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
-iowa_model.fit(train_X, train_y)
-val_predictions = iowa_model.predict(val_X)
-val_mae = mean_absolute_error(val_predictions, val_y)
-print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+# # Using best value for max_leaf_nodes
+# iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+# iowa_model.fit(train_X, train_y)
+# val_predictions = iowa_model.predict(val_X)
+# val_mae = mean_absolute_error(val_predictions, val_y)
+# print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
 
-# Define the model. Set random_state to 1
-rf_model = RandomForestRegressor(random_state=1)
-rf_model.fit(train_X, train_y)
-rf_val_predictions = rf_model.predict(val_X)
-rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
-print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+# # Define the model. Set random_state to 1
+# rf_model = RandomForestRegressor(random_state=1)
+# rf_model.fit(train_X, train_y)
+# rf_val_predictions = rf_model.predict(val_X)
+# rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+# print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+
+
+# # Import the necessary modules and libraries
+# import numpy as np
+# from sklearn.tree import DecisionTreeRegressor
+# import matplotlib.pyplot as plt
+
+# # Create a random dataset
+# rng = np.random.RandomState(1)
+# X = np.sort(5 * rng.rand(80, 1), axis=0)
+# y = np.sin(X).ravel()
+# y[::5] += 3 * (0.5 - rng.rand(16))
+
+# # Fit regression model
+# regr_1 = DecisionTreeRegressor(max_depth=2)
+# regr_2 = DecisionTreeRegressor(max_depth=5)
+# regr_1.fit(X, y)
+# regr_2.fit(X, y)
+
+# # Predict
+# X_test = np.arange(0.0, 5.0, 0.01)[:, np.newaxis]
+# y_1 = regr_1.predict(X_test)
+# y_2 = regr_2.predict(X_test)
+
+# # Plot the results
+# plt.figure()
+# plt.scatter(X, y, s=20, edgecolor="black", c="darkorange", label="data")
+# plt.plot(X_test, y_1, color="cornflowerblue", label="max_depth=2", linewidth=2)
+# plt.plot(X_test, y_2, color="yellowgreen", label="max_depth=5", linewidth=2)
+# plt.xlabel("data")
+# plt.ylabel("target")
+# plt.title("Decision Tree Regression")
+# plt.legend()
+# plt.show()

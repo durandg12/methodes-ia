3d3ec1e655938036145fe8d42961d44e98fa58d5
Alicia_LEMAIRE <109146419+Aliciiaa@users.noreply.github.com>
2024-03-24 08:32:36 +0100

    pour garder les infos

--- viz_images.py
+++ viz_images.py
@@ -1,26 +1,72 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import NN_code
 
-def viz_im(model, nb, in_channels, out_channels, h):
-     """
-     Fonction pour visualiser le résultat des prédictions sur le jeu de données test.
+def viz_im(model, nb, testloader, file = None, architecture, input_channels, output_channels,  h, residual_block, end_activation, p, optimizer, criterion, device, lr, trainloader, n_epochs = 10):
+   """
+      Fonction pour visualiser le résultat des prédictions sur le jeu de données test.
 
-     Parameters
-     ----------
-     model : nn.Module
-        The trained model.
-     nb : int
-        L'indice de l'image sur laquelle on souhaite tester notre modèle.
+      Parameters
+      ----------
+      model : nn.Module
+         The trained model.
+      nb : int
+         The index of the batch_size on which we want to test our model.
+      testloader : torch.utils.data.DataLoader
+         The test Dataloader.
+      architecture : function
+         The architecture of the neural network to be trained
+      in_channels : int
+         Number of input channels, 1 (black & white) or 3 (RGB).
+      out_channels : int
+         The number of classes in our multinomial distribution (256), the value a pixel can take.
+      h : int
+         Number of channels in the layers.
+      residual_block : function
+         The residual block to be used in our architecture, which defines the model.
+      end_activation : torch.nn
+         The activation function to be used at the end of our architecture.
+      p : int
+         Number of residual blocks.
+         5 if PixelCNN
+         6 if PixelRNN
+      optimizer : torch.optim.optimizer.Optimizer
+         The optimizer to use
+      criterion : nn.modules._Loss
+         The loss function to minimize
+      device : string
+         The device to use.
+      lr : float
+         The learning rate
+      trainloader : torch.utils.data.DataLoader
+         The train DataLoader
+      n_epochs : int, default = 10
+         Number of epochs to do
 
-     Returns
-     -------
-     """
-#Création du modèle
-model2 = Architecture_Pixel(in_channels, out_channels, h, ResidualBlock_CNN, nn.Softmax(dim=0)).to(device = device) #création d'une instance de la classe
-model2.load_state_dict(torch.load('PixelCNN_MNIST.pth')) #importation des poids
-model2.eval()
+      
 
-# Génération de l'image
-image, _ = testset[nb] #on récupère une image du test set
-image_true = np.ravel(image.numpy())
+      Returns
+      -------
+   """
+   
+   if file != None:
+       #Create an instance of the class
+      model = Architecture_Pixel(in_channels, out_channels, h, ResidualBlock_CNN, nn.Softmax(dim=0)).to(device = device)
+      model.load_state_dict(torch.load('file')) #importation des poids
+       #on our files : h = 5, p = 3, lr = 0.09, optimizer = Adam, epoch = 10/15
+   else :
+       model = train_loop(architecture, input_channels, output_channels,  h, residual_block, end_activation, p, optimizer, criterion, device, lr, trainloader, n_epochs = 10)
+
+
+   # Génération de l'image
+   batch_image, _ = testloader[nb] #on récupère une image du test set
+   batch_image_true = np.ravel(batch_image.numpy())
 
 # Afficher l'image du test set
 image_true = image_true.reshape(28, 28)
@@ -54,7 +100,7 @@ plt.title('image prédite avec argmax')
 plt.show()
 
 
-#Si h petit et nombre d'epoque petit, on n'a seulement des pixels noirs (0) et blancs (253) (h = 10, epoch = 10)
+
 # Sampling
 
 sample = np.zeros(784)
@@ -64,10 +110,7 @@ for i in range(784):
 
 
         indices = torch.multinomial(probs, 1)
-        #counts = torch.bincount(indices)
 
-        # Trouver l'indice qui correspond à la classe la plus fréquente
-        #sample[ i] = torch.argmax(counts)
         sample[ i] = indices
 
 
